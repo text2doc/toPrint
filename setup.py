7b2330fa9fefc3a671039fbfbfa3a287d1fefe29
Tom Softreck <info@softreck.dev>
2025-06-08 11:53:15 +0200

    Release version 0.1.12
    ### Added
    - Changes in .version
    - Changes in AUTHORS.txt
    - Changes in CHANGELOG.md
    - Changes in CLASSIFIERS.txt
    - Changes in CNAME
    - Changes in SCRIPTS.txt
    - Changes in changelog.py
    - Changes in git.sh
    - Changes in increment_init.py
    - Changes in increment_project.py
    - Changes in increment_setup.py
    - Changes in increment_version.py
    - Changes in project.sh
    - Changes in publish.sh
    - Changes in requirements.txt
    - Changes in setup.cfg
    - Changes in setup.py
    - Changes in setup.py.bak
    - Changes in upgrade.sh
    - Changes in version.sh
    
    ### Changed
    - Changes in LICENSE
    - Changes in README.md

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except FileNotFoundError:
 
 setup(
     name="toprint",
-    version="0.1.31",
+    version="0.1.32",
     description="2print/toprint: Python library for printing and converting between HTML, PDF, ZPL, and image formats with printer support. Also known as 2print or to-print.",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -21,16 +21,59 @@ setup(
     keywords=[
         # Podstawowe słowa kluczowe
         'printing', 'pdf', 'html', 'zpl', 'label', 'printer', 'conversion',
-        # Warianty z 2 dla lepszego wyszukiwania
-        '2print', '2-print', 'print2', 'print-2',
-        # Konwersje między formatami
-        'html2pdf', 'pdf2print', 'zpl2print', 'image2print',
-        'html-to-pdf', 'pdf-to-print', 'zpl-to-print', 'image-to-print',
-        # Dodatkowe słowa kluczowe
-        'document', 'report', 'label-printing', 'barcode', 'qrcode',
-        'thermal-printing', 'document-generation', 'report-generation',
+        
+        # Warianty nazwy
+        '2print', '2-print', 'print2', 'print-2', 'toprint', 'to-print',
+        
+        # Konwersje HTML
+        'html2print', 'html-to-print', 'html2pdf', 'html-to-pdf',
+        'html2zpl', 'html-to-zpl', 'html2img', 'html-to-image',
+        'html2doc', 'html-to-doc', 'html2docx', 'html-to-docx',
+        'html2md', 'html-to-md', 'html2markdown', 'html-to-markdown',
+        'html2xml', 'html-to-xml', 'html2json', 'html-to-json',
+        
+        # Konwersje PDF
+        'pdf2print', 'pdf-to-print', 'pdf2html', 'pdf-to-html',
+        'pdf2zpl', 'pdf-to-zpl', 'pdf2img', 'pdf-to-image',
+        'pdf2doc', 'pdf-to-doc', 'pdf2docx', 'pdf-to-docx',
+        'pdf2md', 'pdf-to-md', 'pdf2markdown', 'pdf-to-markdown',
+        'pdf2xml', 'pdf-to-xml', 'pdf2json', 'pdf-to-json',
+        
+        # Konwersje ZPL
+        'zpl2print', 'zpl-to-print', 'zpl2pdf', 'zpl-to-pdf',
+        'zpl2html', 'zpl-to-html', 'zpl2img', 'zpl-to-image',
+        'zpl2doc', 'zpl-to-doc', 'zpl2docx', 'zpl-to-docx',
+        'zpl2md', 'zpl-to-md', 'zpl2markdown', 'zpl-to-markdown',
+        'zpl2xml', 'zpl-to-xml', 'zpl2json', 'zpl-to-json',
+        
+        # Konwersje obrazów
+        'img2print', 'image-to-print', 'img2pdf', 'image-to-pdf',
+        'img2html', 'image-to-html', 'img2zpl', 'image-to-zpl',
+        'img2doc', 'image-to-doc', 'img2docx', 'image-to-docx',
+        'img2md', 'image-to-md', 'img2markdown', 'image-to-markdown',
+        'img2xml', 'image-to-xml', 'img2json', 'image-to-json',
+        
+        # Konwersje dokumentów
+        'doc2print', 'doc-to-print', 'doc2pdf', 'doc-to-pdf',
+        'doc2html', 'doc-to-html', 'doc2zpl', 'doc-to-zpl',
+        'doc2img', 'doc-to-image', 'doc2docx', 'doc-to-docx',
+        'doc2md', 'doc-to-md', 'doc2markdown', 'doc-to-markdown',
+        'doc2xml', 'doc-to-xml', 'doc2json', 'doc-to-json',
+        
+        # Konwersje Markdown
+        'md2print', 'markdown-to-print', 'md2pdf', 'markdown-to-pdf',
+        'md2html', 'markdown-to-html', 'md2zpl', 'markdown-to-zpl',
+        'md2img', 'markdown-to-image', 'md2doc', 'markdown-to-doc',
+        'md2docx', 'markdown-to-docx', 'md2xml', 'markdown-to-xml',
+        'md2json', 'markdown-to-json',
+        
+        # Dodatkowe funkcje
+        'document-conversion', 'file-conversion', 'format-conversion',
+        'document-processing', 'document-generation', 'report-generation',
+        'label-printing', 'barcode', 'qrcode', 'thermal-printing',
         'zebra', 'zpl-printer', 'weasyprint', 'pdf-generation',
-        'print-to-pdf', 'thermal-print', 'label-maker', 'document-conversion'
+        'label-maker', 'document-automation', 'print-automation',
+        'batch-printing', 'bulk-printing', 'template-printing'
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
