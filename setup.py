7cb595fcd2d876ed0537ad89286f332b651755db
Tom Softreck <info@softreck.dev>
2025-06-08 11:48:00 +0200

    Release version 0.1.12
    ### Added
    - Changes in .version
    - Changes in AUTHORS.txt
    - Changes in CHANGELOG.md
    - Changes in CLASSIFIERS.txt
    - Changes in CNAME
    - Changes in SCRIPTS.txt
    - Changes in changelog.py
    - Changes in git.sh
    - Changes in increment_init.py
    - Changes in increment_project.py
    - Changes in increment_setup.py
    - Changes in increment_version.py
    - Changes in project.sh
    - Changes in publish.sh
    - Changes in requirements.txt
    - Changes in setup.cfg
    - Changes in setup.py
    - Changes in setup.py.bak
    - Changes in upgrade.sh
    - Changes in version.sh
    
    ### Changed
    - Changes in LICENSE
    - Changes in README.md

--- setup.py
+++ setup.py
@@ -1,18 +1,66 @@
 from setuptools import setup, find_packages
 
+# Read long description from README.md
+try:
+    with open("README.md", "r", encoding="utf-8") as fh:
+        long_description = fh.read()
+except FileNotFoundError:
+    long_description = "A comprehensive printing solution for HTML, PDF, ZPL, and image formats with printer support"
+
 setup(
-    name="toPrint",
+    name="to-print",
     version="0.1.26",
-    description="Print from html to pdf, zpl, image, printer: html to print, htmltoPrint, html to pdf,html2pdf, pdf to print, pdftoPrint, zpl to print,zpltoPrint, image to print,  imagetoPrint, ",
+    description="Python library for printing and converting between HTML, PDF, ZPL, and image formats with printer support",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Tom Softreck",
     author_email="info@softreck.dev",
-    url="https://github.com/toPrint/toPrint",
+    url="https://github.com/text2doc/toPrint",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
+    keywords=[
+        'printing', 'pdf', 'html', 'zpl', 'label', 'printer', 'conversion',
+        'html2pdf', 'pdf2print', 'zpl2print', 'image2print', 'document',
+        'report', 'label-printing', 'barcode', 'qrcode', 'thermal-printing',
+        'document-generation', 'report-generation', 'zebra', 'zpl-printer',
+        'weasyprint', 'pdf-generation', 'html-to-pdf', 'print-to-pdf',
+        'thermal-print', 'label-maker', 'document-conversion'
+    ],
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Manufacturing',
+        'Intended Audience :: System Administrators',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Printing',
+        'Topic :: Text Processing :: Markup :: HTML',
+        'Topic :: Multimedia :: Graphics :: Graphics Conversion',
+        'Topic :: Office/Business :: Office Suites',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Operating System :: OS Independent',
     ],
     python_requires=">=3.7",
+    install_requires=[
+        'weasyprint>=53.0',
+        'Pillow>=8.0.0',
+        'reportlab>=3.6.0',
+    ],
+    project_urls={
+        'Bug Reports': 'https://github.com/text2doc/toPrint/issues',
+        'Source': 'https://github.com/text2doc/toPrint',
+        'Documentation': 'https://github.com/text2doc/toPrint#readme',
+        'Changelog': 'https://github.com/text2doc/toPrint/releases',
+    },
+    entry_points={
+        'console_scripts': [
+            'to-print=toPrint.cli:main',
+        ],
+    },
 )
